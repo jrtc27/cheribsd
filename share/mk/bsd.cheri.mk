d64a08b544f25235a71fcf8fe43a8a97717f8e91
James Clarke <jrtc27@jrtc27.com>
2017-04-26 19:34:16 +0100

    Add new CHERI_USE_MCT option, defaulting to yes

--- share/mk/bsd.cheri.mk
+++ share/mk/bsd.cheri.mk
@@ -64,6 +64,9 @@ _CHERI_CC+= -Wno-deprecated-declarations
 OBJCOPY:=	objcopy
 MIPS_ABI=	purecap
 _CHERI_CC+=	-mxgot -fpic
+.if ${MK_CHERI_USE_MCT} == "no"
+_CHERI_CC+=	-mno-mct
+.endif
 LIBDIR:=	/usr/libcheri
 ROOTOBJDIR=	${.OBJDIR:S,${.CURDIR},,}${SRCTOP}/worldcheri${SRCTOP}
 CFLAGS+=	${CHERI_OPTIMIZATION_FLAGS:U-O2}
